cad02a203534a7e25312ccc20eb1885b8f5629b2
Bhavin Jawade <bhavinjawade@gmail.com>
2020-11-06 02:01:28 -0500

    mobile friendly homepage

---
+++ server/chemml/chemml/wrapper/sklearn_skl/KernelRidge_wrapper.py
@@ -0,0 +1,26 @@
+"""
+A wrapper for the sklearn.kernel_ridge.KernelRidge
+"""
+from chemml.wrapper.interfaces import evaluate_inputs
+from sklearn.kernel_ridge import KernelRidge
+
+class KernelRidge:
+    def __init__(self,block,stack):
+
+        # evaluate function inputs
+        inputs = evaluate_inputs(block['inputs'], stack)
+
+        # run function
+        self.clf = KernelRidge(**inputs['kwargs'])
+        self.clf.fit(*inputs['args'])
+    
+    
+
+    def predict_y(self, X_test):
+        y_predict= self.clf.predict(X_test)
+
+        return y_predict
+
+
+# model = KernelRidge(block, stack)
+# model.predict_y(x)
